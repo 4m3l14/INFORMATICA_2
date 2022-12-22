9b17a226f8424956d173aca3e2964d706b24d292
4m3l14 <113897169+4m3l14@users.noreply.github.com>
2022-12-22 21:27:01 +0100

    Update esercizio.c

--- DES02-FileBinari/esercizio.c
+++ DES02-FileBinari/esercizio.c
@@ -1,15 +1,18 @@
-/******************************************************************************************
+/*******************************************************************************************
 * \mainpage record dei file
 *
-* @brief inserisce record carica e stampa dati dello studente, ricerca tramite cognome, correggi				
+* @brief inserisce record carica dati degli studenti e stampa dati dello studente, ricerca tramite cognome,
+*        stampa informazioni nel record passato come parametro, correggi, numero record presenti nel file				
 * @author 		Wannaku Amelia
-* @date 		29/11/2022
+* @date 		22/12/2022
 * @version 1.0 	29/11/2022 Versione iniziale
+* @version 2.0 	22/12/2022 Versione finale
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 #define C 20
 #define V 8
 #define N 2
@@ -46,17 +49,16 @@ int correggiRecord(char [], int ); /*visualizza le informazioni del record richi
  
 int numeroRecord(char []); //restituisce il numero di record presenti nel file.
 
-
 int main()
 {
-	char nomeFile[]={"test.dat"};
-	int r, s, a, dim=2;
+	char nomeFile[]={"file.dat"};
+	int r, s, a;
 	char st[C];
 	do{
 		s=menu();	
 		switch(s){
 			case 1: 
-				printf("\nInserire quanti record vuoi. Inserisci un intero\n");
+				printf("\nInserire quanti record vuoi\n");
 				scanf("%d", &r);
 				InserisciRecord(nomeFile, r);	
 				break;
@@ -66,36 +68,39 @@ int main()
 			case 3:	
 				printf("\nInserisci il cognome da cercare\n");
 				scanf("%s", st);
-				r=ricercaRecord(nomeFile, st);		
-				if(r!=1){	
-					printf("\nErrore, record non trovato");
+				r=ricercaRecord(nomeFile, st);	
+				//se la funzione non restituisce 1, visualizza errore	
+				if(r!=1){            	
+					printf("\nErrore, record non trovato\n");
 				}
 				break;
 			case 4:	
 				printf("\nInserisci la posizione \n");
 				scanf("%d", &a);
-				r=stampaRecord(nomeFile, a);	
+				r=stampaRecord(nomeFile, a);
+				//la funzione restituisce -1 se il record non e' presente	
 				if(r==-1){
-					printf("\nErrore, record non trovato");
+					printf("\nErrore, record non trovato\n");
 				}
 				break;
 			case 5:	
 				printf("\nInserire la posizione del record da correggere\n");
 				scanf("%d", &a);
 				r=correggiRecord(nomeFile, a);
+				//la funzione restituisce -1 se il record non e' presente	
 				if(r==-1){
-					printf("\nErrore, record non trovato");
+					printf("\nErrore, record non trovato\n");
 				}
 				break;
 			case 6:	
 				r=numeroRecord(nomeFile);	
-				printf("\nSono presenti %d record nel file", r);
+				printf("\nSono presenti %d record nel file\n", r);
 				break;
 			case 0:	
-				printf("\nCiao!");				
+				printf("\nArrivederci!");				
 				break;
 			default:	
-				printf("Errore!");	
+				printf("Errore\n!");	
 				break;			
 		}	
 	}while(s!=0);
@@ -119,9 +124,11 @@ void InserisciRecord(char fileName[], int numRecord)
 	studente buffer;
 	int i, j, r;
 	
-	pFile=fopen(fileName,"ab");
+	//Apertura del file binario in modalita' append
+	pFile=fopen(fileName,"ab"); 
 	
 	srand(time(NULL));
+	//controllo del file, che sia aperto senza errori
 	if(pFile!=NULL){
 		for(i=0;i<numRecord;i++){
 			printf("Inserisci il cognome dello studente:\n");
@@ -136,8 +143,10 @@ void InserisciRecord(char fileName[], int numRecord)
 		for(j=0;j<V;j++){
 			buffer.voti[j]=1+rand()%10;
 		}
+		//fwrite funzione che ci permette di scrivere sul file binario
 		fwrite(&buffer,sizeof(studente), 1, pFile);
-	fclose(pFile);
+		fclose(pFile);
+	}
 }
 
 
@@ -154,16 +163,19 @@ void stampaFile(char fileName[])
 	studente buffer;
 	int n, j;
 	
+	//apertura del file binario in lettura
 	pFile=fopen(fileName,"rb");
 	
+	//controllo del file, che sia aperto senza errori
 	if(pFile!=NULL){
+		//finche' il file non finisce
 		while(!feof(pFile)){
 			n=fread(&buffer, sizeof(studente), 1, pFile);
 		
 			if(n>0){
 				printf("Cognome dello studente: %s\t",buffer.cognome);
 				printf("E' nato il %d/%s/%d\t",buffer.nascita.giorno,buffer.nascita.mese,buffer.nascita.anno);
-				printf("I voti sono:\t");
+				printf("\nI voti sono:\t");
 				for(j=0;j<V;j++){	
 					printf("%d\t",buffer.voti[j]);            
 				}
@@ -173,7 +185,7 @@ void stampaFile(char fileName[])
 	fclose(pFile);	
 	}	
 	else
-		printf("Errore nell'apertura del file");
+	printf("Errore nell'apertura del file");
 }
 
 /** ****************************************************************************************
@@ -189,18 +201,22 @@ int ricercaRecord (char fileName[], char cognome[])
 {
 	FILE *pFile;
 	studente buffer;
-	int i, n=0, media=0, eta=0, anno=2022;
+	int i, n, media, eta, anno=2022;
 	
+	//apertura del file binario in lettura
 	pFile=fopen(fileName,"rb");
 	
+	//controllo del file, che sia aperto senza errori
 	if(pFile!=NULL){
+		//finche' il file non finisce
 		while(!feof(pFile)){
+			//funzione fread permette di leggere i dati che l'utente ha passato come parametro
 			n=fread(&buffer, sizeof(studente), 1, pFile);
 			if(strcmp(buffer.cognome,cognome)==0){
 				printf("Cognome dello studente: %s\n",buffer.cognome);
-				printf("e' nato il %d/%d/%d\n",buffer.nascita.giorno,buffer.nascita.mese,buffer.nascita.anno);
+				printf("e' nato il %d/%s/%d\n",buffer.nascita.giorno,buffer.nascita.mese,buffer.nascita.anno);
 				eta=anno-buffer.nascita.anno;
-				printf("eta':%d\n",eta);
+				printf("eta': %d\n",eta);
 			}	
 			for(i=0; i<V; i++){
 				media=media+buffer.voti[i];
@@ -211,12 +227,12 @@ int ricercaRecord (char fileName[], char cognome[])
 		fclose(pFile);
 		return 1;
 	}	
+	//restituisce -1 se c'e' un errore nell' apertura del file
 	else{
 		printf("Errore nell'apertura del file");
 		return -1;
 	}		
 }
-
 
 /** ****************************************************************************************
 * @brief stampa le informazioni presenti nel record specificato 
@@ -232,7 +248,7 @@ int stampaRecord(char fileName[], int posizione)
 {
 	FILE *pFile;
 	studente buffer;
-	int n;
+	int n,j;
 	
 	pFile=fopen(fileName,"rb");
 	//apertura del file senza problemi
@@ -252,8 +268,8 @@ int stampaRecord(char fileName[], int posizione)
 			fclose(pFile);  
 			return 0;     
 		}
+		//se il record non viene letto restituisce -1
 		else{
-		//se il record non viene letto
 		return -1;
 		}
 	}
@@ -262,7 +278,6 @@ int stampaRecord(char fileName[], int posizione)
 		return -1;
 	}
 }
-
 
 
 /** ****************************************************************************************
@@ -278,12 +293,16 @@ int correggiRecord(char fileName[], int posizione)
 {
 	FILE *pFile;
 	studente buffer;
-	pFile=fopen("file.dat","rb+");
+	//apertura file binario in lettura/scrittura
+	pFile=fopen(fileName,"rb+");
 	int r, n;
 	
+	//controllo del file, che sia aperto senza errori
 	if(pFile!=NULL){
-		fseek(pFile, posizione*sizeof(buffer), SEEK_SET);
+		//funzione fseek posizionare il puntatore del File nella posizione desiderata 
+		fseek(pFile, posizione*sizeof(buffer), SEEK_SET); 
 		n=fread(&buffer,sizeof(studente),1,pFile);
+		//chiamata della funzione stampaRecord
 		r=stampaRecord(fileName, posizione);
 		if(n>0){
 			printf("Inserisci il cognome dello studente:\n");
@@ -294,7 +313,9 @@ int correggiRecord(char fileName[], int posizione)
 			scanf("%s",buffer.nascita.mese);
 			printf("Inserisci l'anno di nascita dello studente:\n");
 			scanf("%d",&buffer.nascita.anno);
+			//funzione fseek posizionare il puntatore del File nella posizione desiderata 
 			fseek(pFile, posizione*sizeof(buffer), SEEK_SET);
+			//fwrite funzione che ci permette di scrivere sul file binario
 			fwrite(&buffer,sizeof(studente),1,pFile);
 			fclose(pFile);
 			return 0;
@@ -302,15 +323,12 @@ int correggiRecord(char fileName[], int posizione)
 		else
 		return -1;			
 	}
-	else
-	{
+	else{
 		printf("Errore in apertura file");
 		return -1;
 	}	
 }
 	
-
-
 
 /** ****************************************************************************************
 * @brief restituisce il numero di record presenti nel file.
@@ -328,9 +346,11 @@ int numeroRecord(char fileName[])
 	studente buffer;	
 	//apertura del file in reading binary mode
 	pFile=fopen(fileName,"rb");	
-	//Controllo che il file si apra senza problemi
+	//controllo che il file senza errori
 	if(pFile!=NULL)	{
+		//finche' il file non finisce
 		while(!feof(pFile))	{
+			//lettura del file binario
 			n=fread(&buffer, sizeof(buffer),1,pFile);	
 			if(n>0){
 				cont++;	
@@ -345,6 +365,7 @@ int numeroRecord(char fileName[])
 	}
 }
 
+//menu
 int menu(void)
 {
 	int x;
@@ -355,7 +376,8 @@ int menu(void)
 	printf("4:stampaRecord\n");
 	printf("5:correggiRecord\n");
 	printf("6:numeroRecord\n");
-	printf("0:Esci");
+	printf("0:Esci\n");
 	scanf("%d",&x);
 	return x;
 }
+
