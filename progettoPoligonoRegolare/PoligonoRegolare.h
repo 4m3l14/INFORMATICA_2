241b8cd4222741739d3e95ba6872928778b40b0b
4m3l14 <113897169+4m3l14@users.noreply.github.com>
2023-02-21 19:08:56 +0100

    Create PoligonoRegolare.h

---
+++ progettoPoligonoRegolare/PoligonoRegolare.h
@@ -0,0 +1,54 @@
+class PoligonoRegolare
+{
+	private:
+		int numeroLati;
+		double MisuraLato;
+		
+	public:
+		PoligonoRegolare();
+		PoligonoRegolare(int,double);
+		int getnumeroLati();
+		void setnumeroLati(int);
+		double getMisuraLato();
+		void setMisuraLato(double);
+		double perimetro();
+	
+};
+
+class Rombo:public PoligonoRegolare
+{
+	private:
+		double DiagonaleMaggiore;
+	public:
+		Rombo();
+		Rombo(MisuraLato,DiagonaleMaggiore);
+		double getDiagonaleMaggiore();
+		void setDiagonaleMaggiore(double);
+	    double DiagonaleMinore();
+	    double perimetro();
+	    double area();
+};
+
+class Quadrato:public Rombo
+{
+
+	public:
+	
+		Quadrato();
+		Quadrato(int);
+		
+		double area();
+		double perimetro();
+};
+
+class TriangoloEquilatero:public PoligonoRegolare
+{
+	private:
+		double altezza;
+	public:
+		TriangoloEquilatero();
+		TriangoloEquilatero(int);
+		
+		double area();
+		double perimetro();
+};
